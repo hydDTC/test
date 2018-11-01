9a961f490c318cb1b0bbef02258ac2ae2375406d
moka <gaoyuanfell@sina.com>
2018-11-01 14:55:16 +0800

    `$date`

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@
 echo "Start to publish..."
 git add .
 date=$(date '+%Y%m%d %H:%M:%S')
-git commit -am '$date'
+git commit -am '`$date`'
 git pull origin master
 git push origin master
 echo "Success";
