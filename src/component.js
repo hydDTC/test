5105c9dbcbb0789cd9c0687542d06fe42b8e28bb
moka <gaoyuanfell@sina.com>
2018-11-07 17:57:50 +0800

    2018-11-07 17:57:50 → moka

--- src/component.js
+++ src/component.js
@@ -5,6 +5,7 @@ import login from './components/login.vue';
 import actionSheet from './components/actionSheet.vue';
 import modal from './components/modal.vue';
 import switchInput from './components/switchInput.vue';
+import previewImg from './components/previewImg.vue';
 
 export default function (Vue) {
     Vue.component(header.name, header);
@@ -14,4 +15,5 @@ export default function (Vue) {
     Vue.component(actionSheet.name, actionSheet);
     Vue.component(modal.name, modal);
     Vue.component(switchInput.name, switchInput);
+    Vue.component(previewImg.name, previewImg);
 }
---
+++ src/components/previewImg.vue
@@ -0,0 +1,73 @@
+<template>
+  <div ref="box" class="preview-img-box">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "preview-img",
+  props: {
+    time:{
+      type: Number,
+      default: 2000,
+    }
+  },
+  data() {
+    return {
+      number: 0,
+      width: 0,
+      x: 0,
+      index: 0,
+      timerNumber: 0,
+      destroyed: false,
+    };
+  },
+  computed: {},
+  created() {},
+  mounted() {
+    let children = Array.from(this.$refs.box.children);
+    this.number = children.length;
+    children.forEach(item => {
+      item.style.width = item.clientWidth + 'px'
+    })
+    let width = this.width = children.map(a => a.clientWidth).reduce((a, b) => a + b);
+    this.$refs.box.style.width = width + 'px'
+    this.$refs.box.style.display = 'flex'
+    if(this.number > 1){
+      this.timer()
+    }
+  },
+  methods: {
+    timer(){
+      this.timerNumber = setInterval(()=> {
+        if(this.destroyed) return;
+        this.next()
+      }, this.time)
+    },
+    pre(){
+      let _x = this.width / this.number;
+      --this.index
+      if(this.index < 0) this.index = this.number - 1
+      this.$refs.box.style.transform = `translateX(-${this.index * _x}px)`
+    },
+    next(){
+      let _x = this.width / this.number;
+      ++this.index
+      if(this.index > this.number - 1) this.index = 0
+      this.$refs.box.style.transform = `translateX(-${this.index * _x}px)`
+    }
+  },
+  destroyed(){
+    this.destroyed = true;
+    clearInterval(this.timerNumber)
+  }
+};
+</script>
+<style lang="less" scoped>
+.preview-img-box{
+  transition: all 0.5s;
+  transform: translateX(0);
+}
+</style>
+
+
